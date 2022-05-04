7f786cf350a00d7c35f1aa59e3638a7b482f155b
cory <14861205+coryc5@users.noreply.github.com>
2022-05-04 18:19:39 -0500

    init elixir app

---
+++ lib/papa/application.ex
@@ -0,0 +1,20 @@
+defmodule Papa.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    children = [
+      # Starts a worker by calling: Papa.Worker.start_link(arg)
+      # {Papa.Worker, arg}
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Papa.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
